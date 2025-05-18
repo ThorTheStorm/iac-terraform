c874678e93cb67d19a1259ad2c0353e71e428746
Tor Egil Berntsen <t.e.g.b@outlook.com>
2025-05-18 22:00:30 +0200

    update

--- course_materials/module07/besvarelse/providers.tf
+++ course_materials/module07/besvarelse/providers.tf
@@ -14,7 +14,7 @@ terraform {
         resource_group_name  = "rg-tbtisip-backend-tfstate-dev-ne"                        # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
         storage_account_name = "sttfstate001dnuutfrn"                  # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
         container_name       = "sctfstate001"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-        key                  = "ghactions-m07.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+        key                  = "ghactions-m07-${terraform.workspace}.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
     } # This block is used to configure the backend for storing the Terraform state file.
 
 }
